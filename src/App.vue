adbfd523b1a0df083cdbee0e9e68f3d509fc9c82
SerhBondarenko <serhii.bondarenk@gmail.com>
2022-06-26 23:20:04 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<div><button @click="addLike" >Like</button> <button @click="addDislike">Dislike</button></div>
+     <div>Кількість лайків: <strong>{{ likes }}</strong>  </div>
+    <div>Кількість дизлайків: <strong>{{ dislikes }}</strong>  </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() { 
+        return{
+            likes: 0,
+            dislikes: 2,
+        }
+    },
+    methods: {
+        addLike() {this.likes += 1},
+        addDislike() {this.dislikes += 1},
+    }
+}
+</script>
+
+<style>
+
+</style>
